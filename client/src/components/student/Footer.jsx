e14d2ab879a92028f9bf9e1395dc932565cfba0d
ADEE8080 <adiabloiio@gmail.com>
2025-04-08 13:18:24 +0530

    first commit

---
+++ client/src/components/student/Footer.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <footer className='bg-pink-700 md:px-36 text-left w-full mt-10'>
+      <div className='flex flex-col md:flex-row items-start px-8 md:px-0 justify-center gap-10 
+      md:gap-32 py-10 border-b border-white/30'>
+        <div className='flex flex-col md:items-start items-center w-full'>
+
+          <div className="flex items-center space-x-2">
+                <img
+                  onClick={()=>navigate('/')}
+                  src={assets.logo_dark}
+                  alt="Logo"
+                  className="w-14 lg:w-16 cursor-pointer"
+                />
+                 <p className="text-lg lg:text-xl font-semibold text-white">TutorSync</p>
+                </div>
+
+          <p className='mt-6 text-center md:text-left text-sm text-white/80'>"TutorSync is your go-to platform for personalized learning, connecting you with expert tutors and curated study resources. 
+         <br/>Learn at your own pace, discover the right content, and achieve your academic goals effortlessly. 
+          "</p>
+        </div>
+        <div className='flex flex-col md:items-start items-center w-full'>
+          <h2 className='font-semibold text-white mb-5'>Company</h2>
+          <ul className='flex md:flex-col w-full justify-between text-sm text-white/80 md:space-y-2'>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+          </ul>
+        </div>
+        <div className='hidden md:flex flex-col items-center w-full'>
+          <h2 className='font-semibold text-white mb-5'>Subscribe to our newsletter</h2>
+          <p className='text-sm text-white/80'>The latest news, articles, and resources, sent to your inbox weekly</p>
+          <div className='flex items-center gap-2 pt-4'>
+            <input type="email" placeholder='Enter your email' className='border border-gray-500/30 bg-gray-800 text-gray-500 placeholder-gray-500 
+            outline-none w-64 h-9 rounded px-2 text-sm' />
+            <button className='bg-blue-600 w-24 h-9 text-white rounded'>Subscribe</button>
+          </div>
+
+        </div>
+
+      </div>
+      <p className='py-4 text-center text-xs md:text-sm text-white/60'>Copyright 2025 @ Adiabloiio. All Right Reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
